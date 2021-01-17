0a21a66f3c9739ec665898d89e580fad3f5ef22f
Rimso <abdeldebeche@gmail.com>
2021-01-18 00:09:59 +0100

    can play medias but not youtube video

--- src/modules/feed/services/post.mapper.ts
+++ src/modules/feed/services/post.mapper.ts
@@ -9,38 +9,55 @@ export class PostMapper {
   }
 
   private parseMessage(message: string): PostMessage {
-    // TODO rajouter png jpg et gif
-    const pictureRegex = /http[s]?:\/\/.+\.(jpeg|jpg)/gmi;
+    // rajouter png jpg et gif
+    const pictureRegex = /http[s]?:\/\/\S+\.(jpeg|jpg|png|gif)/gmi;
 
-     // TODO mp4,wmv,flv,avi,wav
-    const videoRegex = / /gmi;
+     // mp4,wmv,flv,avi,wav
+    const videoRegex = /http[s]?:\/\/\S+\.(mp4|wmv|flv|wav|avi)/gmi;
 
-     // TODO mp3,ogg,wav
-    const audioRegex = / /gmi;
+     // mp3,ogg,wav
+    const audioRegex = /http[s]?:\/\/\S+\.(mp3|ogg|wav)/gmi;
 
-    const youtubeRegex = /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
+    const youtubeRegex = /(http[s]?:\/\/)?(?:www\.youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
     const attachements: MessageElement[] = [];
 
-    const pictureMatche = pictureRegex.exec(message);
-    if (pictureMatche) {
-     // TODO ajouter un attachement de type image dans attachements
+    let pictureMatche = null;
+    while (pictureMatche = pictureRegex.exec(message)) {
+      const img : MessageImageElement = {
+        type: 'image',
+        url: pictureMatche[0]
+      } ;
+      attachements.push(img);
     }
 
-    const videoMatche = videoRegex.exec(message)
-    if (videoMatche) {
-     // TODO ajouter un attachement de type video dans attachements
-
+    let videoMatche =null;
+    while (videoMatche = videoRegex.exec(message)) {
+     //  ajouter un attachement de type video dans attachements
+     const vid : MessageVideoElement = {
+      type: 'video',
+      url: videoMatche[0]
+    } ;
+    attachements.push(vid);
     }
 
-    const audioMatche = audioRegex.exec(message)
-    if (audioMatche) {
-     // TODO ajouter un attachement de type audio dans attachements
-
+    let audioMatche = null;
+    while (audioMatche = audioRegex.exec(message)) {
+     //  ajouter un attachement de type audio dans attachements
+     const aud : MessageAudioElement = {
+      type: 'audio',
+      url: audioMatche[0]
+    } ;
+    attachements.push(aud);
     }
 
-    const youtubeMatche = youtubeRegex.exec(message)
-    if (youtubeMatche) {
-     // TODO ajouter un attachement de type youtube dans attachements
+    let youtubeMatche = null;
+    while (youtubeMatche= youtubeRegex.exec(message)) {
+     //  ajouter un attachement de type youtube dans attachements
+     const yt : MessageYoutubeElement = {
+      type: 'youtube',
+      videoId: youtubeMatche[2]
+    } ;
+    attachements.push(yt);
     }
 
     return {
