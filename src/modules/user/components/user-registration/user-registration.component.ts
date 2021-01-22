5e94ec9175c4940ee7a55e89937585eb9711c874
Abdel Rahim Debeche <abdel-rahim.debeche.jv@valeo.com>
2021-01-22 17:35:55 +0100

    Username used error message

--- src/modules/user/components/user-registration/user-registration.component.ts
+++ src/modules/user/components/user-registration/user-registration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { LocalUserQueries } from '../../services/platform/local/user.queries.local';
+import { UserLocalStorage } from '../../services/platform/local/user.storage';
 import { UserService } from '../../services/user.service';
 
 class UserRegistrationFormModel {
@@ -12,7 +15,8 @@ class UserRegistrationFormModel {
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
-  styleUrls: ['./user-registration.component.less']
+  providers: [ LocalUserQueries ],
+  styleUrls: ['./user-registration.component.less' ]
 })
 export class UserRegistrationComponent implements OnInit {
   @ViewChild("f")
@@ -22,23 +26,37 @@ export class UserRegistrationComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private localUserQueries: LocalUserQueries,
+    private message: NzMessageService,
   ) { }
 
   ngOnInit(): void {
+  }
+
+  createMessage(type: string): void {
+    this.message.create(type, `This is a message of ${type}`);
   }
 
   async submit() {
 
     // Vérifier que la confirmation de mot de passe correspond au mot de passe
     if (this.form.form.invalid || this.model.password !== this.model.confirmPassword) {
-      alert("Les mots de passe ne sont pas identique");
+      // this.model.confirmPassword.
       return;
     }
+    this.localUserQueries.exists(this.model.username).then(exist =>{
+      if(exist)
+      {
+        this.message.error("Ce nom d'utilisateur n'est pas disponilble");
+        return;
+      }
 
-    // Enregistrer l'utilisateur via le UserService
-    this.userService.register(this.model.username, this.model.password);
-    this.goToLogin();
+      // Enregistrer l'utilisateur via le UserService
+      this.userService.register(this.model.username, this.model.password);
+      this.goToLogin();
+    });
+    
   }
 
   goToLogin() {
